60c3a7b1fbdaa92b8d6a3545c2d4621740c5e4b4
Ellen Fairholm <elliefairholm@gmail.com>
2023-05-11 13:36:37 +0100

    feat: updated api URL to use new deployment

--- on-this-day.js
+++ on-this-day.js
@@ -13,7 +13,7 @@ Module.register("on-this-day", {
 
   defaults: {
     updateInterval: undefined, // the fact will update daily at 00:00, if you want it to be updated more regularly specify a time in ms
-    apiBase: "http://on-this-day-api.herokuapp.com",
+    apiBase: "https://on-this-day-api.fly.dev",
     path: "/facts",
     animationSpeed: 1000,
     interests: ["general"],
